2e29fa3d33a89715ce3244c52aa24f15022ef7db
Czarina <castilloczarina@gmail.com>
2023-03-26 23:30:35 +0800

    Modified html file and created js file

---
+++ sketch.js
@@ -0,0 +1,4 @@
+let api = 'https://api.currencyfreaks.com/v2.0/rates/latest?apikey=YOUR_APIKEY'
+const fromDropDown = document.getElementById("from-currency-select");
+const toDropDown = document.getElementById("to-currency-select");
+
